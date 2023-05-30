a55981c44c52cfa79af1a724cbdace9c4e191cf7
Mahmoud Khalifa <mahmoudbkhalifa@gmail.com>
2023-05-30 17:15:22 +0100

    Updated Footer

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -4,8 +4,12 @@ const Listings = () => {
   return (
     <div className="w-full py-[10rem] px-4 bg-white">
     <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+    <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div className="w-full flex justify-center items-center flex-col mb-7">
     <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2 flex justify-center items-center">Get started</h1>
-    <p className="flex justify-center items-center">Leverage our fast-growing community to do more events.</p>
+    <p className='py-4'>Leverage our fast-growing community to do more events.</p>
+    </div>
+    </div>
     </div>
     </div>
   )
